d2836047830cd2f5179d2581dbc736542b14ddcb
Alexandre Henrique <alexandre_eaw@hotmail.com>
2022-05-20 18:44:43 -0300

    project Morfeu

---
+++ morfeu/src/App.js
@@ -0,0 +1,16 @@
+import Homepage from '../src/assets/LoginPage.jpg';
+import './App.css';
+import LoginPage from './components/LoginPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <LoginPage Homepage/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ morfeu/src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import styles from "./LoginPage.module.css"
+const LoginPage = () => {
+  return (
+    <div className={styles.img_logo}>   
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
