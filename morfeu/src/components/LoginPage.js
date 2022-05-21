dc7406c4f3d65755dc5b26ae83c0738cb0811289
Alexandre Henrique <83831769+DevAlexandre1@users.noreply.github.com>
2022-05-21 17:15:35 -0300

    Continue project

--- morfeu/src/components/LoginPage.js
+++ morfeu/src/components/LoginPage.js
@@ -1,14 +1,28 @@
 import styles from "./LoginPage.module.css"
-// const backImgLogin = new URL("./assets/LoginPage.jpg", import.meta.url)
+const backImgLogin = new URL("./assets/LoginPage.jpg", import.meta.url)
 //const imgLogin = new URL("../assets/HomePageButton.jpg")
 const LoginPage = () => {
   return (
     <section className={styles.main_log}>
       <div >           
-          <div>
-            {/* <img src={imgLogin} alt="imagem-login" /> */}
-             <h1>Teste</h1>
-          </div>
+          <form className={styles.formLogin}>
+            <div>
+
+
+            </div>
+              <div >
+                  <label htmlFor="email">Email: </label>   
+                  <input type="email" name="email" id="email" placeholder="Digite seu email" />
+              </div>
+              <div>
+                  <label htmlFor="senha">Senha: </label>
+                  <input type="password" name="senha" id="senha" placeholder="Informe sua senha"/>
+              </div>
+              <div>
+                <input type="submit" value="Enviar" />
+              </div>
+              
+          </form>
       </div>
     </section>
   )
