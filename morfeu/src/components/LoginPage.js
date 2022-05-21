7b48d81a6456a6f3ada2af7d554eb13bfdc00a4b
Alexandre Henrique <83831769+DevAlexandre1@users.noreply.github.com>
2022-05-21 18:47:16 -0300

    Update LoginPage.js

--- morfeu/src/components/LoginPage.js
+++ morfeu/src/components/LoginPage.js
@@ -1,22 +1,42 @@
 import styles from "./LoginPage.module.css"
 import LoginImg from "../assets/logoBrancoLogin.png"
-// const backImgLogin = new URL("./assets/LoginPage.jpg", import.meta.url)
-//const imgLogin = new URL("../assets/HomePageButton.jpg")
+import {useState} from 'react'
+
 const LoginPage = () => {
-  return (
+  
+  //Criando o gerenciador de dados do form
+  
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
+
+  const handleEmail = (e) => {
+    setEmail(e.target.valeu);
+  }
+  const handleSenha = (el) => {
+    setSenha(el.target.valeu);
+  }
+const handleSubmit = (event) =>{
+  event.preventDefault();
+  console.log("enviando o formulario")
+  console.log(email)
+  console.log(senha)
+}
+  
+  return (  
+
     <section className={styles.main_log}>
       <div >           
-          <form className={styles.formLogin}>
+          <form className={styles.formLogin} onSubmit={handleSubmit}>
             <div className ={styles.imgLogin}>
                 <img src={LoginImg} alt="ImagemLogin" />
             </div>
               <div >
                   <label htmlFor="email">Email: </label>   
-                  <input type="email" name="email" id="email" placeholder="Digite seu email" />
+                  <input type="email" name="email" id="email" placeholder="Digite seu email" onChange={handleEmail} />
               </div>
               <div>
                   <label htmlFor="senha">Senha: </label>
-                  <input type="password" name="senha" id="senha" placeholder="Informe sua senha"/>
+                  <input type="password" name="senha" id="senha" placeholder="Informe sua senha" onChange={handleSenha}/>
               </div>
               <div >
                 <input className={styles.buttonEnviar}  type="submit" value="Enviar" />
