88b0bbd64a96fe78266862bebc1a3ed79ba22c55
Alexandre Henrique <alexandre_eaw@hotmail.com>
2022-06-09 16:24:23 -0300

    Arrumando botao adicionar

--- morfeu/src/pages/ControleAtividades.js
+++ morfeu/src/pages/ControleAtividades.js
@@ -23,8 +23,7 @@ const addTask=()=>{
         <body className={styles.container}>  
             <div className={styles.btn_add_div}>
                 <button className={styles.btn_addTask} onClick={addTask} type="submit">Add task</button>     
-                <button className={styles.btn_addTask}  type="submit">Start All</button>    
-                <button className={styles.btn_addTask}  type="submit">Stop All</button>    
+                 
             </div>
             <div className={styles.container_dashboard}><h1>Dashboard</h1></div>          
             <div className={styles.containerTasks}>
